6fc0eab968d8ff844fb0ff51ddd1a17a57427342
Alexander Muth <almu.hp@arcor.de>
2020-04-28 02:51:42 +0200

    Added missing compatibility of C++ API with C API

--- private/macros.h
+++ private/macros.h
@@ -4,19 +4,19 @@
 #include "exports.h"
 
 #ifdef __cplusplus
-#   include "cpp/macros.h"
+    #include "cpp/macros.h"
 #else
-#   include "c/macros.h"
+    #include "c/macros.h"
 #endif
 
 /* private plugin initialization function macro */
 #ifndef PRIVATE_APLUGINSDK_INIT_FUNCTION
-#   define PRIVATE_APLUGINSDK_INIT_FUNCTION void PRIVATE_APLUGINSDK_API_NAMESPACE initAPlugin()
+    #define PRIVATE_APLUGINSDK_INIT_FUNCTION void PRIVATE_APLUGINSDK_API_NAMESPACE initAPlugin()
 #endif
 
 /* private plugin finalization function macro*/
 #ifndef PRIVATE_APLUGINSDK_FINI_FUNCTION
-#   define PRIVATE_APLUGINSDK_FINI_FUNCTION void PRIVATE_APLUGINSDK_API_NAMESPACE finiAPlugin()
+    #define PRIVATE_APLUGINSDK_FINI_FUNCTION void PRIVATE_APLUGINSDK_API_NAMESPACE finiAPlugin()
 #endif
 
 #endif /* APLUGINSDK_MACROS_H */
