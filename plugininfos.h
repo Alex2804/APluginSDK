f8c21a4af64d28f5d7e9cded367a22eff9083d86
Alexander Muth <almu.hp@arcor.de>
2020-05-31 20:17:09 +0200

    Refactored

--- plugininfos.h
+++ plugininfos.h
@@ -9,55 +9,53 @@ PRIVATE_APLUGINSDK_OPEN_PRIVATE_NAMESPACE
 PRIVATE_APLUGINSDK_CLOSE_PRIVATE_NAMESPACE
 
 PRIVATE_APLUGINLIBRARY_OPEN_NAMESPACE
-    PRIVATE_APLUGINSDK_OPEN_EXTERN_C
-        struct APluginInfo;
+    struct APluginInfo;
 
-        struct APluginFeatureInfo
-        {
-            const struct APluginInfo* pluginInfo;
-            const char* featureGroup;
-            const char* featureName;
-            const char* returnType;
-            const char* parameterList;
-            void* functionPointer;
-        };
+    enum APluginLanguage
+    {
+        CPP, C
+    };
 
-        struct APluginClassInfo
-        {
-            const struct APluginInfo* pluginInfo;
-            const char* interfaceName;
-            const char* className;
-            void* createInstance;
-            void* deleteInstance;
-        };
+    struct APluginFeatureInfo
+    {
+        const struct APluginInfo *pluginInfo;
+        const char *featureGroup;
+        const char *featureName;
+        const char *returnType;
+        const char *parameterList;
+        void *functionPointer;
+    };
 
-        enum APluginLanguage
-        {
-            CPP, C
-        };
+    struct APluginClassInfo
+    {
+        const struct APluginInfo *pluginInfo;
+        const char *interfaceName;
+        const char *className;
+        void *createInstance;
+        void *deleteInstance;
+    };
 
-        struct APluginInfo
-        {
-            struct PRIVATE_APLUGINSDK_PRIVATE_NAMESPACE APrivatePluginInfo* privateInfo;
+    struct APluginInfo
+    {
+        struct PRIVATE_APLUGINSDK_PRIVATE_NAMESPACE APrivatePluginInfo* privateInfo;
 
-            size_t apiVersionMajor, apiVersionMinor, apiVersionPatch;
+        size_t apiVersionMajor, apiVersionMinor, apiVersionPatch;
 
-            void*(*allocateMemory)(size_t size);
-            void(*freeMemory)(void*);
+        void*(*allocateMemory)(size_t size);
+        void(*freeMemory)(void*);
 
-            enum APluginLanguage pluginLanguage;
-            char* pluginName;
-            size_t pluginVersionMajor, pluginVersionMinor, pluginVersionPatch;
+        enum APluginLanguage pluginLanguage;
+        char *pluginName;
+        size_t pluginVersionMajor, pluginVersionMinor, pluginVersionPatch;
 
-            size_t(*getFeatureCount)();
-            const struct APluginFeatureInfo*(*getFeatureInfo)(size_t index);
-            const struct APluginFeatureInfo * const*(*getFeatureInfos)();
+        size_t(*getFeatureCount)();
+        const struct APluginFeatureInfo*(*getFeatureInfo)(size_t index);
+        const struct APluginFeatureInfo * const*(*getFeatureInfos)();
 
-            size_t(*getClassCount)();
-            const struct APluginClassInfo*(*getClassInfo)(size_t index);
-            const struct APluginClassInfo* const*(*getClassInfos)();
-        };
-    PRIVATE_APLUGINSDK_CLOSE_EXTERN_C
+        size_t(*getClassCount)();
+        const struct APluginClassInfo*(*getClassInfo)(size_t index);
+        const struct APluginClassInfo* const*(*getClassInfos)();
+    };
 PRIVATE_APLUGINLIBRARY_CLOSE_NAMESPACE
 
 #endif /* APLUGINSDK_PLUGININFOS_H */
--- private/macros.h
+++ private/macros.h
@@ -1,6 +1,8 @@
 #ifndef APLUGINSDK_MACROS_H
 #define APLUGINSDK_MACROS_H
 
+#include "../libs/ACUtils/include/ACUtils/macros.h"
+
 #include "exports.h"
 
 #ifdef __cplusplus
@@ -8,6 +10,9 @@
 #else
 #   include "c/macros.h"
 #endif
+
+#define PRIVATE_APLUGINSDK_OPEN_EXTERN_C ACUTILS_OPEN_EXTERN_C
+#define PRIVATE_APLUGINSDK_CLOSE_EXTERN_C ACUTILS_CLOSE_EXTERN_C
 
 /* private plugin initialization function macro */
 #ifndef PRIVATE_APLUGINSDK_INIT_FUNCTION
